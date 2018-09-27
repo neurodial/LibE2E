d26d2d9db64c5f765c0241ecc22177bb0c440c87
Kay Gawlik <kay.gawlik@charite.de>
2018-09-27 11:23:10 +0200

    Lizenz hinzugefügt

--- E2E/datadict/e2emdbdata.cpp
+++ E2E/datadict/e2emdbdata.cpp
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2018 Kay Gawlik <kaydev@amarunet.de> <kay.gawlik@beuth-hochschule.de> <kay.gawlik@charite.de>
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// TODO: this file need a complete rewrite
+//       replace the huge switch through a map structure with the E2E-ID as key and the object to a factory object
+
+
 #include "e2emdbdata.h"
 #include "e2emdbdir.h"
 
---
+++ README.md
@@ -0,0 +1,13 @@
+# LibE2E
+
+a library for read the nativ file format from HEYEX (Heidelberg Engineering) (pdb, edb, sdb, E2E).
+
+Note that the reader for this file format is reverse engineered and incomplete.
+
+## License
+
+This project is licensed under the LGPL3 License - see [license.txt](license.txt) file for details
+
+## Build
+
+for build instructions see the readme from the OCT-Marker project
